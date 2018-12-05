476d82c8462f2cbbc91ad54b2e0c9968edfcd92a
yujinee <yujinee132@gmail.com>
2018-12-05 15:48:43 +0900

    add colorscheme

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ rm -rf ~/.vimrc
 rm -rf ~/.vim/
 cp .vimrc ~/
 
-#cp Tomorrow-Night-Bright.vim /usr/share/vim/vim74/color/
+cp Tomorrow-Night-Bright.vim /usr/share/vim/vim74/colors/
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
