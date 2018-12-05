2006bfa84c5d1ccc251070663264889a54706f16
yujinee <yujinee132@gmail.com>
2018-12-05 15:53:59 +0900

    use local path

--- setup.sh
+++ setup.sh
@@ -6,9 +6,11 @@ rm -rf ~/.vimrc
 rm -rf ~/.vim/
 cp .vimrc ~/
 
-cp Tomorrow-Night-Bright.vim /usr/share/vim/vim74/colors/
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
 
 vim +PlugInstall +qall 
+
+mkdir -p ~/.vim/colors
+cp Tomorrow-Night-Bright.vim ~/.vim/colors/
